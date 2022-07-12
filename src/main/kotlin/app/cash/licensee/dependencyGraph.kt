3534910b8f3a6539225838fa8b9ffee638175cb5
Jake Wharton <github@jakewharton.com>
2022-07-12 08:17:39 -0400

    Update src/main/kotlin/app/cash/licensee/dependencyGraph.kt

--- src/main/kotlin/app/cash/licensee/dependencyGraph.kt
+++ src/main/kotlin/app/cash/licensee/dependencyGraph.kt
@@ -159,7 +159,7 @@ private fun loadDependencyCoordinates(
 
 private fun Project.pomConfiguration(
   pomDependency: Dependency,
-  variants: List<ResolvedVariantResult>
+  variants: List<ResolvedVariantResult>,
 ) = configurations
   .detachedConfiguration(pomDependency)
   .apply {
